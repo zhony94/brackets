856c73e5fd83c524b2f9dd1311a035b42807ec3c
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-02-19 13:09:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str) {
+  const holder = []
+  let count = 0
+  const BracketsSame = ['|','1','2','3','4','5','6','7','8']
+  const openBrackets = ['(','{','[']
+  const closedBrackets = [')','}',']']
+  for (let letter of str) {
+    if(BracketsSame.includes(letter)){
+      count = count + 1
+    }else{
+      if(openBrackets.includes(letter)){ 
+        holder.push(letter)
+      }else if(closedBrackets.includes(letter)){ 
+        const openPair = openBrackets[closedBrackets.indexOf(letter)] 
+        if(holder[holder.length - 1] === openPair){ 
+            holder.splice(-1,1) 
+        }else{ 
+            holder.push(letter)
+            break 
+        }
+      }
+    }        
+  }
+  return (holder.length === 0 && count%2 === 0) 
 }
